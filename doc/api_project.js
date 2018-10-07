77f9474f988703d4e5f7894b8b06b225b6b0c66b
Rahul Trivedi <rahul.trivedi2@hp.com>
2018-10-08 00:50:17 +0530

    API Docs

---
+++ doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Message",
+  "version": "1.0.0",
+  "description": "A sample rest api",
+  "title": "Messages",
+  "url": "https://<your.web.site>",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-07T19:19:05.875Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
